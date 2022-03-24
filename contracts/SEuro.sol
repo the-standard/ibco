7c48db565ace57951376ea971ed9b00782d065e8
Ewan Sheldon <ewansheldon@gmail.com>
2022-03-24 17:05:58 +0000

    adds SEURO contract

---
+++ contracts/SEuro.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract SEuro is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address[] memory _admins
+    ) ERC20(name, symbol) {
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(BURNER_ROLE, msg.sender);
+
+        for (uint8 i = 0; i < _admins.length; i++) {
+            _setupRole(MINTER_ROLE, _admins[i]);
+            _setupRole(BURNER_ROLE, _admins[i]);
+        }
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "invalid-minter");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        require(hasRole(BURNER_ROLE, msg.sender), "invalid-burner");
+        _burn(from, amount);
+    }
+}
