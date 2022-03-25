e2b7a6c4ca9ebe1c6c67457a04af135aeed19588
Ewan Sheldon <ewansheldon@gmail.com>
2022-03-25 10:38:34 +0000

    buy weth with mock

--- test/ibco.js
+++ test/ibco.js
@@ -1,23 +1,29 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai')
+const { ethers, network } = require('hardhat');
+const { expect } = require('chai');
 
 describe('IBCO', async () => {
     const WETH_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-    let IBCO, WETH, SEuro, owner, user;
+    let IBCO, SEuro, WETH, owner, user, benefactor;
+
+    async function buyWETH(signer, amount) {
+        await WETH.connect(signer).deposit({value: amount});
+    }
 
     beforeEach(async () => {
-        [owner, user] = await ethers.getSigners();
+        [owner, user, benefactor] = await ethers.getSigners();
         const IBCOContract = await ethers.getContractFactory('IBCO');
         IBCO = await IBCOContract.deploy();
         const SEuroContract = await ethers.getContractFactory('SEuro');
         SEuro = await SEuroContract.deploy('SEuro', 'SEUR', [owner.address]);
-        WETH = await ethers.getContractAt('IERC20', WETH_ADDRESS);
+        WETH = await ethers.getContractAt('WETH', WETH_ADDRESS);
     });
 
     describe('swap', async () => {
         it('swaps for given token', async () => {
             const toSwap = await ethers.utils.parseEther('1');
             const wethBytes = await ethers.utils.formatBytes32String('WETH');
+            await buyWETH(user, toSwap);
+
             await IBCO.swap(wethBytes, toSwap);
 
             const userSEuroBalance = await SEuro.balanceOf(user.address);
