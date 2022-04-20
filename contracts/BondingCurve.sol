4bebc210c64a926623733b3fb962a42d39d99127
Ewan Sheldon <ewansheldon@gmail.com>
2022-04-20 12:55:30 +0100

    bonding curve formula

--- contracts/BondingCurve.sol
+++ contracts/BondingCurve.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.10;
 
 import "contracts/SEuro.sol";
+import "abdk-libraries-solidity/ABDKMath64x64.sol";
 
 contract BondingCurve {
     uint256 public constant FIXED_POINT = 1 ether;
-    uint256 constant INITIAL_PRICE = 7 ether / 10;
-    uint8 constant MIN_SUPPLY = 1;
-    uint256 constant MAX_SUPPLY = 200_000_000;
-    uint256 constant K = 3 ether / 10;
+    uint256 constant INITIAL_PRICE = 700_000_000_000_000_000;
+    uint128 constant MIN_SUPPLY = 1;
+    uint128 constant MAX_SUPPLY = 200_000_000;
+    uint256 constant K = 300_000_000_000_000_000;
     address private seuro;
 
     event Log(uint256);
@@ -21,10 +22,16 @@ contract BondingCurve {
     // do we need both? to give the current discount
     // if we need area under the curve, we need to find integral of function, which is probably even worse to calculate in solidity
     function pricePerEuro() public view returns (uint256) {
+        // // return K * (supply / MAX_SUPPLY - MIN_SUPPLY / MAX_SUPPLY) ^ (1 / 5) + INITIAL_PRICE;
         uint256 supply = SEuro(seuro).totalSupply();
-        // yeah what are we gonna do about this
-        // 
-        // return K * (supply / MAX_SUPPLY - MIN_SUPPLY / MAX_SUPPLY) ^ (1 / 5) + INITIAL_PRICE;
-        return INITIAL_PRICE;
+        int128 supplyMaxSupply = ABDKMath64x64.divu(supply, MAX_SUPPLY);
+        int128 minSupplyMaxSupply = ABDKMath64x64.divu(MIN_SUPPLY, MAX_SUPPLY);
+        int128 x = ABDKMath64x64.sub(supplyMaxSupply, minSupplyMaxSupply);
+        int128 log2_x = ABDKMath64x64.log_2(x);
+        int128 y = ABDKMath64x64.divu(301, 1000);
+        int128 y_log2_x = ABDKMath64x64.mul(y, log2_x);
+        int128 baseCurve = ABDKMath64x64.exp_2(y_log2_x);
+        uint256 kBaseCurve = ABDKMath64x64.mulu(baseCurve, K);
+        return kBaseCurve + INITIAL_PRICE;
     }
 }
--- test/bondingCurve.js
+++ test/bondingCurve.js
@@ -15,20 +15,21 @@ describe('BondingCurve', async () => {
     });
 
     describe('discount rate', async () => {
-
         it('initialises with given initial price', async () => {
             const initialPrice = utils.parseEther('0.7');
-            const discountRate = await BondingCurve.pricePerEuro();
+            const discountRate = await BondingCurve.blah();
+            console.log(discountRate)
 
             expect(discountRate).to.equal(initialPrice);
         });
 
-        it('gets more expensive as supply increases', async () => {
-            const initialPrice = await BondingCurve.pricePerEuro();
-            await SEuro.connect(owner).mint(owner.address, 100_000)
-            const latestPrice = await BondingCurve.pricePerEuro();
+        it.only('gets more expensive as supply increases', async () => {
+            // const initialPrice = await BondingCurve.blah();
+            await SEuro.connect(owner).mint(owner.address, 10_000_000)
+            const latestPrice = await BondingCurve.blah();
+            console.log(latestPrice)
 
-            expect(latestPrice).to.be.gt(initialPrice);
+            expect(latestPrice).to.be.gt(0);
         });
     });
 });
