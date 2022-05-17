e2ead72d2067608f0491ea83842734c116be4f4a
Ewan Sheldon <ewansheldon@gmail.com>
2022-05-17 12:45:06 +0100

    rename bonding curve vars for readability

--- contracts/BondingCurve.sol
+++ contracts/BondingCurve.sol
@@ -7,35 +7,35 @@ import "abdk-libraries-solidity/ABDKMath64x64.sol";
 contract BondingCurve {
     uint256 public constant FIXED_POINT = 1_000_000_000_000_000_000;
     uint256 private constant FINAL_PRICE = 1_000_000_000_000_000_000;
-    uint8 private constant l = 1;
-    uint8 private constant jNumerator = 1;
-    uint8 private constant jDenominator = 5;
+    uint8 private constant INITIAL_SUPPLY = 1;
+    uint8 private constant J_NUMERATOR = 1;
+    uint8 private constant J_DENOMINATOR = 5;
 
-    uint256 private immutable i;
-    uint256 private immutable m;
+    uint256 private immutable initalPrice;
+    uint256 private immutable maxSupply;
     uint256 private immutable k;
     int128 private immutable j;
     address private immutable seuro;
 
     constructor(address _seuro, uint256 _initialPrice, uint256 _maxSupply) {
         seuro = _seuro;
-        i = _initialPrice;
-        m = _maxSupply;
-        k = FINAL_PRICE - i;
-        j = ABDKMath64x64.divu(jNumerator, jDenominator);
+        initalPrice = _initialPrice;
+        maxSupply = _maxSupply;
+        k = FINAL_PRICE - initalPrice;
+        j = ABDKMath64x64.divu(J_NUMERATOR, J_DENOMINATOR);
     }
     
     function pricePerEuro() public view returns (uint256) {
-        uint256 x = SEuro(seuro).totalSupply();
-        if (x == 0) {
-            return i;
+        uint256 supply = SEuro(seuro).totalSupply();
+        if (supply == 0) {
+            return initalPrice;
         }
-        uint256 xMinusL = x - l;
-        int128 xMinusLOverM = ABDKMath64x64.divu(xMinusL, m);
-        int128 log2XMinusLOverM = ABDKMath64x64.log_2(xMinusLOverM);
-        int128 jLog2XMinusLOverM = ABDKMath64x64.mul(j, log2XMinusLOverM);
-        int128 exp2JLog2XMinusLOverM = ABDKMath64x64.exp_2(jLog2XMinusLOverM);
-        uint256 kExp2JLog2XMinusLOverM = ABDKMath64x64.mulu(exp2JLog2XMinusLOverM, k);
-        return kExp2JLog2XMinusLOverM + i;
+        uint256 mintedSupply = supply - INITIAL_SUPPLY;
+        int128 supplyCompletion = ABDKMath64x64.divu(mintedSupply, maxSupply);
+        int128 log2SupplyCompletion = ABDKMath64x64.log_2(supplyCompletion);
+        int128 jlog2SupplyCompletion = ABDKMath64x64.mul(j, log2SupplyCompletion);
+        int128 baseCurve = ABDKMath64x64.exp_2(jlog2SupplyCompletion);
+        uint256 curve = ABDKMath64x64.mulu(baseCurve, k);
+        return curve + initalPrice;
     }
 }
