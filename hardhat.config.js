55caf34a12129215046c3d5be28678cfac6eabc8
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-05-31 11:15:08 +0100

    deps: remove ^ from main deps and updated solc to 0.8.14

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { INFURA_API_KEY } = process.env;
 
 module.exports = {
-  solidity: "0.8.1",
+  solidity: "0.8.14",
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
