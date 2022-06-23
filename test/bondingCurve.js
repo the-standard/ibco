0a4b4a02b014096c8c7283cd70fe0f97d5bf8f01
Ewan Sheldon <ewansheldon@gmail.com>
2022-06-23 17:36:41 +0100

    test crossing price buckets

--- test/bondingCurve.js
+++ test/bondingCurve.js
@@ -41,7 +41,7 @@ describe('BondingCurve', async () => {
     return seuros;
   }
 
-  describe('seuroValue', async () => {
+  describe.only('seuroValue', async () => {
     it('gets the current value of given euros in terms of seuros', async () => {
       const euros = ethers.utils.parseEther('1000');
 
@@ -61,15 +61,24 @@ describe('BondingCurve', async () => {
       expect(seuros).to.equal(expectedSEuros);
     });
 
+    it('will cross two price buckets when calculating', async () => {
+      const euros = BUCKET_SIZE.mul(2);
+      
+      const seuros = await BondingCurve.callStatic.seuroValue(euros);
+
+      const expectedSEuros = await calculateSEuros(euros);
+      expect(seuros).to.equal(expectedSEuros);
+    });
+
     it('saves new bucket price when supply has changed', async () => {
       const euros = ethers.utils.parseEther('1');
-      const initialBucketPrice = (await BondingCurve.currentBucket()).price;
       await SEuro.mint(owner.address, BUCKET_SIZE);
       // activates updating of current price
       await BondingCurve.seuroValue(euros);
+
       const newBucketPrice = (await BondingCurve.currentBucket()).price;
 
-      expect(newBucketPrice).to.be.gt(initialBucketPrice);
+      expect(newBucketPrice).to.equal(await getBucketPrice(1));
     });
 
     it('will not exceed full price', async () => {
