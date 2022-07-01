f289933af2da093538ccd294faff15cc957c5452
dkf <dannzor@outlook.com>
2022-07-01 20:54:15 +0200

    gateway: begin integration with rewards in TST

--- contracts/StandardTokenGateway.sol
+++ contracts/StandardTokenGateway.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "abdk-libraries-solidity/ABDKMath64x64.sol";
 
-// StandardTokenGateway acts as a standard token data feed with the current:
+// StandardTokenGateway holds TSTs and acts as a standard token data feed with:
 // - price per token in EUR;
 // - amount of obtainable tokens as bonding rewards left
 contract StandardTokenGateway is AccessControl {
-	// Address to the TST contract with a maximum supply of 1 billion tokens
-	address public immutable TST_ADDRESS;
+	// Deployed TST contract on mainnet with a maximum supply of 1 billion tokens
+	address public constant TST_ADDRESS = 0xa0b93B9e90aB887E53F9FB8728c009746e989B53;
+
+	// Address to the contract with a maximum supply of 1 billion tokens
+	address public immutable TOKEN_ADDRESS;
 
 	// Address to the sEURO contract with a varying supply
 	address public immutable SEUR_ADDRESS;
@@ -24,6 +27,13 @@ contract StandardTokenGateway is AccessControl {
 	// Enabled when the price is less than 1
 	bool public inversed;
 
+	// True if the contract is connected to mainnet TST token contract.
+	// Set to false for testing purposes.
+	bool immutable isProduction;
+
+	// The amount of TST tokens that are to be paid out in the future.
+	uint256 public pendingPayout;
+
 	// The amount of TST available to get as bond reward
 	uint256 public bondRewardPoolSupply;
 
@@ -32,11 +42,12 @@ contract StandardTokenGateway is AccessControl {
 
 	bytes32 public constant TST_TOKEN_GATEWAY = keccak256("TST_TOKEN_GATEWAY");
 
-	constructor(address _standardToken, address _seuroToken) {
+	constructor(address _tokenAddress, address _seuroToken) {
 		_setupRole(TST_TOKEN_GATEWAY, msg.sender);
-		TST_ADDRESS = _standardToken;
+		TOKEN_ADDRESS = _tokenAddress;
 		SEUR_ADDRESS = _seuroToken;
 		inversed = true;
+		isProduction = TOKEN_ADDRESS == TST_ADDRESS;
 		tokenPrice = 20; // 0.05 EUR
 		TST_MAX_AMOUNT = one_billion * decimals;
 		bondRewardPoolSupply = TST_MAX_AMOUNT / 2; // half the total supply is available as bond reward
@@ -52,9 +63,21 @@ contract StandardTokenGateway is AccessControl {
 		_;
 	}
 
+
 	function setUnitPrice(uint256 _newPrice, bool _inversed) public onlyGatewayOwner {
 		tokenPrice = _newPrice;
 		inversed = _inversed;
+	}
+
+	function getContractBalance() public view returns (uint256) {
+		address localAddress = address(this);
+		IERC20 token = IERC20(TOKEN_ADDRESS);
+		return token.balanceOf(localAddress);
+	}
+
+	modifier enoughBalance(uint256 _toSend) {
+		require(getContractBalance() > _toSend, "err-insufficient-tokens");
+		_;
 	}
 
 	function getStandardTokenPrice() public view returns (uint256, bool) {
