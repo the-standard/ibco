bd80f33f449cb8abb6d94900242b93802309404f
Ewan Sheldon <ewansheldon@gmail.com>
2022-07-05 14:01:59 +0100

    remove selectedAccounts from frontend deployment

--- scripts/frontendReadyDeployment.js
+++ scripts/frontendReadyDeployment.js
@@ -6,13 +6,13 @@ const HARDHAT_DEFAULT_URL = 'http://127.0.0.1:8545/';
 
 async function main() {
   const contractAddresses = await deployContracts();
-  let selectedAccounts = (await ethers.getSigners()).slice(0,10).map(account => account.address);
-  await contractsFrontendReady(selectedAccounts);
-  const [contractOwner] = selectedAccounts;
+  let accountAddresses = (await ethers.getSigners()).map(account => account.address);
+  await contractsFrontendReady(accountAddresses);
+  const [contractOwner] = accountAddresses;
   const chainId = HARDHAT_DEFAULT_CHAIN;
   const serverURL = HARDHAT_DEFAULT_URL;
 
-  const artifact = { contractAddresses, contractOwner, chainId, serverURL, selectedAccounts };
+  const artifact = { contractAddresses, contractOwner, chainId, serverURL };
   const json = JSON.stringify(artifact);
   fs.writeFileSync('scripts/deploymentArtifact.json', json);
 }
