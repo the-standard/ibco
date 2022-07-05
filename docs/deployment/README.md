a82a4933c1518f8f58d32e51dafdb88f6e830c3e
Ewan Sheldon <ewansheldon@gmail.com>
2022-07-05 14:10:03 +0100

    Deployment scripts (#30)
    * deployment wip
    
    * deployment scripts and test swap eth
    
    * frontend ready deployment script
    
    * deployment for frontend guide
    
    * frontend deployment guide
    
    * frontend deployment guide
    
    * delete unused deployment json
    
    * save addresses to deployment stage to save json lookup
    
    * move deployment docs to separate folder
    
    * set chain id to default localhost chain
    
    * mint mainnet fork account with fake tst + tether
    
    * update docs
    
    * remove timeout from config
    
    * test: abi api testing
    
    * test: abi api testing
    
    * test: abi api testing
    
    * test: abi api testing
    
    * test: abi api testing
    
    * test: abi api testing
    
    * test: abi api testing
    
    * remove selectedAccounts from frontend deployment
    
    Co-authored-by: Simon Morley <309336+simonmorley@users.noreply.github.com>

--- docs/deployment/README.md
+++ docs/deployment/README.md
@@ -1,7 +1,7 @@
 # Deployment
 There a few options for deploying the IBCO contracts.
 ## Deployment for frontend development
-This script deploys to a local fork of the mainnet, with standard parameters. It also prepares and activates the IBCO, so that it is open and ready to use.
+This script deploys to a local fork of the mainnet, with standard parameters. It prepares and activates the IBCO, so that it is open and ready to use. It also mints the local accounts with the dummy USDT and TST tokens.
 
 Firstly, follow [installation](../../README.md#installation) and [mainnet fork setup](../../README.md#mainnet-fork-setup).
 
@@ -15,4 +15,4 @@ Run the frontend dev deployment script in the localhost network:
 ```
 npx hardhat run --network localhost scripts/frontendReadyDeployment.js
 ```
-Compilation artifacts e.g. ABIs will be created in the `artifacts` directory. All the contract addresses, as well as the address of the contract owner and the chain ID of the blockchain, will be saved to `scripts/deploymentArtifact.json`.
+Compilation artifacts e.g. ABIs will be created in the `artifacts` directory. All the contract addresses, as well as the addresses of the local accounts and the chain ID of the blockchain, will be saved to `scripts/deploymentArtifact.json`.
--- scripts/deploymentStages.js
+++ scripts/deploymentStages.js
@@ -1,16 +1,16 @@
 const { ethers } = require('hardhat');
-const fs = require('fs');
 let addresses;
+let DummyTST, DummyUSDT;
 
 const INITIAL_PRICE = ethers.utils.parseEther('0.8');
 const MAX_SUPPLY = ethers.utils.parseEther('200000000');
 const BUCKET_SIZE = ethers.utils.parseEther('100000');
-const BONDING_EVENT_TOKEN = '0xdAC17F958D2ee523a2206206994597C13D831ec7'; // USDT
 const UNISWAP_LIQ_MANAGER = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88';
 const OPERATOR_ADDRESS = ethers.constants.AddressZero; // update this when we have the operator
-const TST_ADDRESS = '0xa0b93B9e90aB887E53F9FB8728c009746e989B53';
 
 const deployContracts = async () => {
+    DummyTST = await (await ethers.getContractFactory('DUMMY')).deploy('Standard Token', 'TST', 0);
+    DummyUSDT = await (await ethers.getContractFactory('DUMMY')).deploy('Tether', 'USDT', 0);
     const SEuro = await (await ethers.getContractFactory('SEuro')).deploy('sEURO', 'SEUR', []);
     const BondingCurve = await (await ethers.getContractFactory('BondingCurve')).deploy(SEuro.address, INITIAL_PRICE, MAX_SUPPLY, BUCKET_SIZE);
     const SEuroCalculator = await (await ethers.getContractFactory('SEuroCalculator')).deploy(BondingCurve.address);
@@ -18,12 +18,14 @@ const deployContracts = async () => {
     const SEuroOffering = await (await ethers.getContractFactory('SEuroOffering')).deploy(
         SEuro.address, SEuroCalculator.address, TokenManager.address, BondingCurve.address
     );
-    const StandardTokenGateway = await (await ethers.getContractFactory('StandardTokenGateway')).deploy(TST_ADDRESS, SEuro.address);
+    const StandardTokenGateway = await (await ethers.getContractFactory('StandardTokenGateway')).deploy(DummyTST.address, SEuro.address);
     const BondStorage = await (await ethers.getContractFactory('BondStorage')).deploy(StandardTokenGateway.address);
     const BondingEvent = await (await ethers.getContractFactory('BondingEvent')).deploy(
-        SEuro.address, BONDING_EVENT_TOKEN, UNISWAP_LIQ_MANAGER, BondStorage.address, OPERATOR_ADDRESS
+        SEuro.address, DummyUSDT.address, UNISWAP_LIQ_MANAGER, BondStorage.address, OPERATOR_ADDRESS
     );
 
+    await DummyTST.deployed();
+    await DummyUSDT.deployed();
     await SEuro.deployed();
     await BondingCurve.deployed();
     await SEuroCalculator.deployed();
@@ -41,7 +43,9 @@ const deployContracts = async () => {
         BondingCurve: BondingCurve.address,
         BondingEvent: BondingEvent.address,
         BondStorage: BondStorage.address,
-        StandardTokenGateway: StandardTokenGateway.address
+        StandardTokenGateway: StandardTokenGateway.address,
+        TST: DummyTST.address,
+        USDT: DummyUSDT.address
     };
 
     return addresses
@@ -53,8 +57,18 @@ const activateSEuroOffering = async () => {
     await SEuroOffering.connect(owner).activate();
 }
 
-const contractsFrontendReady = async () => {
+const mintTokensForAccount = async (accounts) => {
+    const million = ethers.utils.parseEther('1000000');
+    const mints = accounts.map(async account => {
+        await DummyTST.mint(account, million);
+        await DummyUSDT.mint(account, million);
+    })
+    await Promise.all(mints);
+}
+
+const contractsFrontendReady = async (accounts) => {
     await activateSEuroOffering();
+    await mintTokensForAccount(accounts);
 }
 
 module.exports = {
