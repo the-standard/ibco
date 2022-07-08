a012f6bd14ea52ea404fec37c08b071a56bc5c28
Ewan Sheldon <ewansheldon@gmail.com>
2022-07-08 12:01:39 +0100

    add testnet networks to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 require('solidity-coverage')
 require('hardhat-contract-sizer');
 
-const { INFURA_API_KEY, SEPOLIA_PRIVATE_KEY } = process.env;
+const { INFURA_API_KEY, TEST_ACCOUNT_PRIVATE_KEY } = process.env;
+
+let sepoliaAccounts = TEST_ACCOUNT_PRIVATE_KEY ? [TEST_ACCOUNT_PRIVATE_KEY] : [];
 
 module.exports = {
   solidity: "0.8.14",
@@ -17,7 +19,11 @@ module.exports = {
     },
     sepolia: {
       url: 'https://rpc.sepolia.dev',
-      accounts: [`${SEPOLIA_PRIVATE_KEY}`]
+      accounts: sepoliaAccounts
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: sepoliaAccounts
     }
   }
 };
