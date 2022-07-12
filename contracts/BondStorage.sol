ec1fa030ea40aca92610b6533737c67240199e7a
dkf <dannzor@outlook.com>
2022-07-12 16:23:01 +0200

    operator: add stage 2 overview contract

--- contracts/BondStorage.sol
+++ contracts/BondStorage.sol
@@ -226,7 +226,7 @@ contract BondStorage is AccessControl {
 	}
 
 	// Claims the payout in TST tokens by sending it to the user's wallet and resetting the claim to zero.
-	function claimReward() external {
+	function claimReward() public {
 		address user = msg.sender;
 		uint256 rewardAmount = issuedBonds[user].claimAmount;
 		issuedBonds[user].claimAmount = 0;
