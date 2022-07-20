72e57e0a9bc196a86c000311cad432ea5fccbf00
Ewan Sheldon <ewansheldon@gmail.com>
2022-07-20 16:47:40 +0100

    remove unneeded quote access of js objects

--- test/bondingReward.js
+++ test/bondingReward.js
@@ -29,16 +29,16 @@ describe('BondingReward', async () => {
     );
 
     // mint some sEUROs, USDTs, and TSTs
-    await SEuro.connect(owner).mint(customer.address, etherBalances["HUNDRED_MILLION"]);
-    await USDT.connect(owner).mint(customer.address, etherBalances["HUNDRED_MILLION"]);
-    await SEuro.connect(owner).mint(owner.address, etherBalances["ONE_BILLION"]);
-    await USDT.connect(owner).mint(owner.address, etherBalances["ONE_BILLION"]);
-    await TST.connect(owner).mint(TGateway.address, etherBalances["FIVE_HUNDRED_MILLION"]);
+    await SEuro.connect(owner).mint(customer.address, etherBalances.HUNDRED_MILLION);
+    await USDT.connect(owner).mint(customer.address, etherBalances.HUNDRED_MILLION);
+    await SEuro.connect(owner).mint(owner.address, etherBalances.ONE_BILLION);
+    await USDT.connect(owner).mint(owner.address, etherBalances.ONE_BILLION);
+    await TST.connect(owner).mint(TGateway.address, etherBalances.FIVE_HUNDRED_MILLION);
     await TGateway.connect(owner).updateRewardSupply();
 
     // approve the bonding contract to move customer sEUR and USDT funds
-    await SEuro.connect(customer).approve(BondingEvent.address, etherBalances["HUNDRED_MILLION"]);
-    await USDT.connect(customer).approve(BondingEvent.address, etherBalances["HUNDRED_MILLION"]);
+    await SEuro.connect(customer).approve(BondingEvent.address, etherBalances.HUNDRED_MILLION);
+    await USDT.connect(customer).approve(BondingEvent.address, etherBalances.HUNDRED_MILLION);
   });
 
   async function balanceTST() {
