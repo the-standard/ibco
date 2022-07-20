9196a0cba29ce09fc40d76053c44b4f28dbd43a3
Ewan Sheldon <ewansheldon@gmail.com>
2022-07-20 14:59:09 +0100

    rename excess transfer function

--- contracts/BondingEvent.sol
+++ contracts/BondingEvent.sol
@@ -264,7 +264,7 @@ contract BondingEvent is AccessControl {
                 : AddedLiquidityResponse(tokenId, liquidity, amount1, amount0);
     }
 
-    function transferDifferenceToWallet(uint256 _addedAmount, uint256 _desiredAmount) private {
+    function transferExcessToWallet(uint256 _addedAmount, uint256 _desiredAmount) private {
         uint256 excess = _desiredAmount - _addedAmount;
         if (excess > 0 && excessCollateralWallet != address(0)) {
             TransferHelper.safeTransfer(OTHER_ADDRESS, excessCollateralWallet, excess);
@@ -340,7 +340,7 @@ contract BondingEvent is AccessControl {
             mintLiquidityPosition(params);
 
         emit LiquidityAdded(_user, added.tokenId, added.seuroAmount, added.otherAmount, added.liquidity);
-        transferDifferenceToWallet(added.otherAmount, otherAmount);
+        transferExcessToWallet(added.otherAmount, otherAmount);
     }
 
     // We assume that there is a higher layer solution which helps to fetch the latest price as a quote.
