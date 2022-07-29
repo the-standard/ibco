6372b8a4cebc2f1f0d2513fc3fe5dfd7b8db8bdb
dkf <dannzor@outlook.com>
2022-07-29 10:17:36 +0200

    lint: conform all js tests to coding style

--- test/operatorStage2.js
+++ test/operatorStage2.js
@@ -2,7 +2,7 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 const bn = require('bignumber.js');
 const { POSITION_MANAGER_ADDRESS, STANDARD_TOKENS_PER_EUR, DECIMALS, etherBalances, rates, durations, ONE_WEEK_IN_SECONDS, MOST_STABLE_FEE, helperFastForwardTime, DEFAULT_SQRT_PRICE, MIN_TICK, MAX_TICK } = require('./common.js');
-bn.config({ EXPONENTIAL_AT: 999999, DECIMAL_PLACES: 40 })
+bn.config({ EXPONENTIAL_AT: 999999, DECIMAL_PLACES: 40 });
 
 let owner, customer, SEuro, TST, USDT;
 
