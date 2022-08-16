bed39d9d3fba41b1389de23d3c08aa249477f302
Ewan Sheldon <ewansheldon@gmail.com>
2022-08-16 16:52:20 +0200

    remove previous conversion function

--- contracts/Rates.sol
+++ contracts/Rates.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.15;
 
 library Rates {
-
-    function convert(uint256 _amount, uint256 _exchangeRate, bool _inverted) public pure returns (uint256) {
-        return _inverted ? _amount * _exchangeRate : _amount / _exchangeRate;
-    }
-
     function convertDefault(uint256 _amount, uint256 _exchangeRate, uint8 _decimals) public pure returns (uint256) {
         return _amount * _exchangeRate / 10 ** _decimals;
     }
