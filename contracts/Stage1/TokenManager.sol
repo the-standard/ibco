bbbb20d54603232fc13d2534a49d031767b64b8e
dkf <dannzor@outlook.com>
2022-08-18 17:20:34 +0100

    token mngr: replace bytes32 with string to display token symbol

--- contracts/Stage1/TokenManager.sol
+++ contracts/Stage1/TokenManager.sol
@@ -10,7 +10,7 @@ contract TokenManager is Ownable {
 
     Token[] public tokens;
 
-    struct Token { bytes32 name; address addr; uint8 dec; address chainlinkAddr; uint8 chainlinkDec; }
+    struct Token { string symbol; address addr; uint8 dec; address chainlinkAddr; uint8 chainlinkDec; }
 
     /// @param _wethAddress address of WETH token
     /// @param _ethUsdCL address of Chainlink data feed for ETH / USD
@@ -20,14 +20,14 @@ contract TokenManager is Ownable {
     }
 
     // Gets the details for the given token, if it is accepted
-    /// @param _name 32-byte array value representation of the token symbol e.g. "WETH", "USDT"
-    function get(bytes32 _name) external view returns(Token memory token) {
-        for (uint256 i = 0; i < tokens.length; i++) if (tokens[i].name == _name) token = tokens[i];
-        require(token.name != bytes32(0), "err-tok-not-found");
+    /// @param _symbol The token symbol e.g. "WETH", "USDC", "USDT"
+    function get(string memory _symbol) external view returns(Token memory token) {
+        for (uint256 i = 0; i < tokens.length; i++) if (cmpString(tokens[i].symbol, _symbol)) token = tokens[i];
+        require(bytes(token.symbol).length > 0, "err-tok-not-found");
     }
 
     function addDefaultTokens(address _wethAddress, address _ethUsdCL, uint8 _ethUsdCLDec) private {
-        addAcceptedToken(WETH_NAME, _wethAddress, WETH_DEC, _ethUsdCL, _ethUsdCLDec);
+        addAcceptedToken("WETH", _wethAddress, WETH_DEC, _ethUsdCL, _ethUsdCLDec);
     }
 
     // Get an array of all the accepted tokens
@@ -36,13 +36,14 @@ contract TokenManager is Ownable {
     }
 
     // Add a token to the accepted list of tokens
-    /// @param _name 32-byte array value representation of the token symbol e.g. "WETH", "USDT"
+    /// @param _symbol The token symbol e.g. "WETH", "USDT"
     /// @param _addr the address of the token
     /// @param _dec the decimals of the token
     /// @param _chainlinkAddr the address of the token / USD Chainlink data feed
     /// @param _chainlinkDec the number of decimals the Chainlink data feed uses
-    function addAcceptedToken(bytes32 _name, address _addr, uint8 _dec, address _chainlinkAddr, uint8 _chainlinkDec) public onlyOwner {
-        tokens.push(Token(_name, _addr, _dec, _chainlinkAddr, _chainlinkDec));
+    function addAcceptedToken(string memory _symbol, address _addr, uint8 _dec, address _chainlinkAddr, uint8 _chainlinkDec) public onlyOwner {
+        //TODO: only receive the address and then fetch the symbol and decimals
+        tokens.push(Token(_symbol, _addr, _dec, _chainlinkAddr, _chainlinkDec));
     }
 
     function deleteToken(uint256 index) private {
@@ -51,8 +52,12 @@ contract TokenManager is Ownable {
     }
 
     // Remove accepted token from accepted list of tokens
-    /// @param _name 32-byte array value representation of the token symbol e.g. "WETH", "USDT"
-    function removeAcceptedToken(bytes32 _name) public onlyOwner {
-        for (uint256 i = 0; i < tokens.length; i++) if (tokens[i].name == _name) deleteToken(i);
+    /// @param _symbol The token symbol e.g. "WETH", "USDT"
+    function removeAcceptedToken(string memory _symbol) public onlyOwner {
+        for (uint256 i = 0; i < tokens.length; i++) if (cmpString(tokens[i].symbol, _symbol)) deleteToken(i);
+    }
+
+    function cmpString(string memory a, string memory b) private pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
 }
