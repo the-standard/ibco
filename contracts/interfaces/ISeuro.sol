a42b5ae1bbef5da61ca5a6029c7417e19e9d1c94
dkf <dannzor@outlook.com>
2022-08-19 14:33:23 +0100

    seuro: remove now-unused functions

--- contracts/interfaces/ISeuro.sol
+++ contracts/interfaces/ISeuro.sol
@@ -5,6 +5,4 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ISeuro is IERC20 {
     function mint(address to, uint256 amount) external;
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
 }
