8aaabc784c18c4fa93776ba24538a906e30b5f30
dkf <dannzor@outlook.com>
2022-08-19 11:04:06 +0100

    token mngr: fetch symbol and decimal from token the erc20 address

--- contracts/Stage1/TokenManager.sol
+++ contracts/Stage1/TokenManager.sol
@@ -1,63 +1,64 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "contracts/interfaces/ISeuro.sol";
 
 contract TokenManager is Ownable {
+    string[] public tokenSymbols;
+    mapping(string => TokenData) tokenMetaData;
 
-    bytes32 private constant WETH_NAME = bytes32("WETH");
-    uint8 private constant WETH_DEC = 18;
-
-    Token[] public tokens;
-
-    struct Token { string symbol; address addr; uint8 dec; address chainlinkAddr; uint8 chainlinkDec; }
+    struct TokenData { address addr; uint8 dec; address chainlinkAddr; uint8 chainlinkDec; }
 
     /// @param _wethAddress address of WETH token
     /// @param _ethUsdCL address of Chainlink data feed for ETH / USD
     /// @param _ethUsdCLDec number of decimals that ETH / USD data feed uses
     constructor(address _wethAddress, address _ethUsdCL, uint8 _ethUsdCLDec) {
-        addDefaultTokens(_wethAddress, _ethUsdCL, _ethUsdCLDec);
+        addAcceptedToken(_wethAddress, _ethUsdCL, _ethUsdCLDec);
     }
 
     // Gets the details for the given token, if it is accepted
     /// @param _symbol The token symbol e.g. "WETH", "USDC", "USDT"
-    function get(string memory _symbol) external view returns(Token memory token) {
-        for (uint256 i = 0; i < tokens.length; i++) if (cmpString(tokens[i].symbol, _symbol)) token = tokens[i];
-        require(bytes(token.symbol).length > 0, "err-tok-not-found");
+    function get(string memory _symbol) external view returns(TokenData memory) {
+        for (uint256 i = 0; i < tokenSymbols.length; i++) if (cmpString(tokenSymbols[i], _symbol)) return tokenMetaData[_symbol];
+        revert("err-tok-not-found");
     }
 
-    function addDefaultTokens(address _wethAddress, address _ethUsdCL, uint8 _ethUsdCLDec) private {
-        addAcceptedToken("WETH", _wethAddress, WETH_DEC, _ethUsdCL, _ethUsdCLDec);
-    }
-
-    // Get an array of all the accepted tokens
-    function getAcceptedTokens() external view returns (Token[] memory) {
-        return tokens;
+    function getAcceptedTokens() external view returns (string[] memory) {
+        return tokenSymbols;
     }
 
     // Add a token to the accepted list of tokens
-    /// @param _symbol The token symbol e.g. "WETH", "USDT"
     /// @param _addr the address of the token
-    /// @param _dec the decimals of the token
     /// @param _chainlinkAddr the address of the token / USD Chainlink data feed
     /// @param _chainlinkDec the number of decimals the Chainlink data feed uses
-    function addAcceptedToken(string memory _symbol, address _addr, uint8 _dec, address _chainlinkAddr, uint8 _chainlinkDec) public onlyOwner {
-        //TODO: only receive the address and then fetch the symbol and decimals
-        tokens.push(Token(_symbol, _addr, _dec, _chainlinkAddr, _chainlinkDec));
+    function addAcceptedToken(address _addr, address _chainlinkAddr, uint8 _chainlinkDec) public onlyOwner {
+        (string memory symbol, uint8 decimals) = getTokenMetaData(_addr);
+        tokenSymbols.push(symbol);
+        tokenMetaData[symbol] = TokenData(_addr, decimals, _chainlinkAddr, _chainlinkDec);
     }
 
     function deleteToken(uint256 index) private {
-        for (uint256 i = index; i < tokens.length - 1; i++) tokens[i] = tokens[i+1];
-        tokens.pop();
+        for (uint256 i = index; i < tokenSymbols.length - 1; i++) tokenSymbols[i] = tokenSymbols[i+1];
+        tokenSymbols.pop();
     }
 
     // Remove accepted token from accepted list of tokens
     /// @param _symbol The token symbol e.g. "WETH", "USDT"
     function removeAcceptedToken(string memory _symbol) public onlyOwner {
-        for (uint256 i = 0; i < tokens.length; i++) if (cmpString(tokens[i].symbol, _symbol)) deleteToken(i);
+        for (uint256 i = 0; i < tokenSymbols.length; i++) if (cmpString(tokenSymbols[i], _symbol)) deleteToken(i);
+        tokenMetaData[_symbol] = TokenData(address(0), 0, address(0), 0);
     }
 
     function cmpString(string memory a, string memory b) private pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    function getTokenMetaData(address _addr) private view returns (string memory, uint8) {
+        string memory sym = ISeuro(_addr).symbol();
+        require(!cmpString(sym, ""), "err-empty-symbol");
+        uint8 dec = ISeuro(_addr).decimals();
+        require(dec > 0, "err-zero-decimals");
+        return (sym, dec);
     }
 }
--- contracts/interfaces/ISeuro.sol
+++ contracts/interfaces/ISeuro.sol
@@ -1,8 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ISeuro is IERC20 {
     function mint(address to, uint256 amount) external;
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
 }
