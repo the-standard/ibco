17f87f7dc2590133de448c9ba3a5005999b3f16a
dkf <dannzor@outlook.com>
2022-08-19 15:09:40 +0100

    token mngr: make apis usable by non-contracts a.k.a humans

--- contracts/Stage1/TokenManager.sol
+++ contracts/Stage1/TokenManager.sol
@@ -24,11 +24,11 @@ contract TokenManager is Ownable {
         revert("err-tok-not-found");
     }
 
-    function getAcceptedTokens() external view returns (string[] memory) {
+    function getAcceptedTokens() public view returns (string[] memory) {
         return tokenSymbols;
     }
 
-    function getTokenDecimalFor(string memory _symbol) external view returns(uint8) {
+    function getTokenDecimalFor(string memory _symbol) public view returns(uint8) {
         return tokenMetaData[_symbol].dec;
     }
 
@@ -36,7 +36,7 @@ contract TokenManager is Ownable {
         return tokenMetaData[_symbol].chainlinkDec;
     }
 
-    function getTokenAddressFor(string memory _symbol) external view returns(address) {
+    function getTokenAddressFor(string memory _symbol) public view returns(address) {
         return tokenMetaData[_symbol].addr;
     }
 
