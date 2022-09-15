7b85b3419bfbb466aa3e42edf808071d06eb194c
Ewan Sheldon <ewansheldon@gmail.com>
2022-09-15 17:24:03 +0200

    fix compiler pragmas

--- contracts/Stage2/RatioCalculator.sol
+++ contracts/Stage2/RatioCalculator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.7.0;
+pragma solidity 0.7.6;
 
 import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import "@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol";
