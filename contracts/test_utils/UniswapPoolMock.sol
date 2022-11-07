f61d318d1ffac4ef5e18776ddb7ef809f4241066
Ewan Sheldon <ewansheldon@gmail.com>
2022-11-07 11:50:14 +0100

    remove unused vars

--- contracts/test_utils/UniswapPoolMock.sol
+++ contracts/test_utils/UniswapPoolMock.sol
@@ -12,7 +12,7 @@ contract UniswapPoolMock {
 
     function tickSpacing() external pure returns (int24) { return 10; }
 
-    function slot0() external view returns (uint160 sqrtPriceX96, int24, uint16, uint16, uint16, uint8, bool) {
+    function slot0() external view returns (uint160, int24, uint16, uint16, uint16, uint8, bool) {
         return (price,0,0,0,0,0,false);
     }
 }
