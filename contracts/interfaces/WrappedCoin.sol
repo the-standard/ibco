eb0e37a39f7dca5f41f3125f73f0c449fa002639
Ewan Sheldon <ewansheldon@gmail.com>
2022-11-11 11:59:17 +0100

    make weth interface more general for wrapped coins

--- contracts/interfaces/WrappedCoin.sol
+++ contracts/interfaces/WrappedCoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface WETH is IERC20 {
+interface WrappedCoin is IERC20 {
     function deposit() external payable;
     function withdraw(uint256 wad) external;
 }
